9b5e1469496b3a62b17e78f781eb578d1410d1e6
LiliRosales <90700177+LiliRosales@users.noreply.github.com>
2021-11-14 20:39:46 -0800

    Se instalo express y se creo archivo server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const route = express.Router();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Conexion a la base de datos
